84c01d33ab8187ebc8188162dab140b3ce1ef3b4
ddd <724987481@qq.com>
2017-06-23 19:15:33 +0800

    添加zsh配色脚本

--- installTemplate.sh
+++ installTemplate.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 #快捷键方式安装
+#需要手动设置
+#chsh -s /bin/zsh  #设置zsh为默认的shell命令
 
 root=`pwd`
 zshDIR=${root}/Util/oh-my-zsh
@@ -10,8 +12,18 @@ cd ~/
 pwd
 target=".zshrc"
 ln -fs ${template} ${target}
-#需要手动设置
-#chsh -s /bin/zsh  #设置zsh为默认的shell命令
+
+#vim 配色 http://www.cnblogs.com/gslyyq/p/5251331.html
+#Vim 的配色最好和终端的配色保持一致，不然在 Terminal/iTerm2 里使用命令行 Vim 会很别扭：
+solarizedDIR=${root}/Util/solarized
+cd $solarizedDIR/vim-colors-solarized/colors
+#新建colors目录，拷贝solarized.vim到新目录中
+mkdir -p ~/.vim/colors && cp solarized.vim ~/.vim/colors/
+echo -e 'syntax enable\nset background=dark\ncolorscheme solarized' >> ~/.vimrc
+
+
+
+
 
 #官方教程安装即目录路径
 #zsh=`pwd`/Util/oh-my-zsh/templates/zshrc.zsh-template
