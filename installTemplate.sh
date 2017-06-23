b370fd6d50d017125ffbc896f21f1d257bfbdaf7
ddd <724987481@qq.com>
2017-06-23 18:34:02 +0800

    添加开启安装zsh模版的插件功能

--- installTemplate.sh
+++ installTemplate.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
-#快捷键
+#快捷键方式安装
+
 root=`pwd`
-template=${root}/templates/zshrc.zsh-template
+zshDIR=${root}/Util/oh-my-zsh
+template=$zshDIR/templates/zshrc.zsh-template
 echo $template
-cd ../
+sed -i '' "s#export ZSH=.*#export ZSH='$zshDIR'#g" $template
+cd ~/
 pwd
 target=".zshrc"
 ln -fs ${template} ${target}
+#需要手动设置
+#chsh -s /bin/zsh  #设置zsh为默认的shell命令
+
+#官方教程安装即目录路径
+#zsh=`pwd`/Util/oh-my-zsh/templates/zshrc.zsh-template
+#cp $zsh ~/.zshrc  #新建私有的配置文件
+
+
